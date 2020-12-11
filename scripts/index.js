6af6663bd419d48e9f057302885c5c5b3a89747d
Egor Repyakh <egor.repahhh@gmail.com>
2020-12-12 00:27:06 +0300

    Правки после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
-const editbutton = document.querySelector('.profile__edit-button');
-const closebutton = document.querySelector('.popup__close-button');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButtonProfile = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup');
-const name = document.getElementById('name');
+const profileName = document.getElementById('name');
 const description = document.getElementById('description');
 const popupName = document.getElementById('popup-name');
 const popupDescription = document.getElementById('popup-description');
@@ -19,44 +19,18 @@ const popupImageName = document.querySelector('.popup__figcaption');
 const popupImageLink = document.querySelector('.popup__image');
 const popupOpenImage = document.querySelector('.popup_pic');
 const popupCloseImage = document.querySelector('.popup__close-image-button');
+const cardTemplate = document.querySelector('#card-template').content;
 
+composeFirstCards();
 
-const initialCards = [
-    {
-        name: 'Балаково',
-        link: 'https://balakovo24.ru/wp-content/uploads/2019/07/погода_балаково.jpg'
-    },
-    {
-        name: 'Архангельск',
-        link: 'http://sail-friend.ru/gallery/3211_17_04_16_8_56_13.jpeg'
-    },
-    {
-        name: 'Санкт-Петербург',
-        link: 'https://gullivertour.ru/assets/images/City/sankt-peterburg/s1200.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-]; 
-
-function firstCards() {
-    const listCards = initialCards.map(compilationCard);
+function composeFirstCards() {
+    const listCards = initialCards.map(createCard);
     cardList.append(...listCards);
 } // Созданиче начальных карточек
 
    
 
-function compilationCard({name, link}) {
-    const cardTemplate = document.querySelector('#card-template').content;
+function createCard({name, link}) {
     const newCard = cardTemplate.cloneNode(true);
     const cardTitle = newCard.querySelector('.element__title');
     const cardLink = newCard.querySelector('.element__photo');
@@ -66,15 +40,15 @@ function compilationCard({name, link}) {
         evt.target.classList.toggle('element__like-button_active');
     });
     newCard.querySelector('.element__delete-button').addEventListener('click', function (evt) {
-        const Card = evt.target.closest('.element');
-        Card.remove();
+        const card = evt.target.closest('.element');
+        card.remove();
     });
 
     cardLink.addEventListener('click', function (evt) {
-        const Card = evt.target.closest('.element');
-        const PopupNameImg = Card.querySelector('.element__title').textContent;
-        const PopupLinkImg = Card.querySelector('.element__photo').src;
-        popupImageOpen({ name: PopupNameImg, link: PopupLinkImg });
+        const card = evt.target.closest('.element');
+        const popupNameImg = card.querySelector('.element__title').textContent;
+        const popupLinkImg = card.querySelector('.element__photo').src;
+        popupImageOpen({ name: popupNameImg, link: popupLinkImg });
     });
     return newCard;
 }
@@ -86,27 +60,27 @@ function popupImageOpen({ name, link }) {
 }
 
 popupCloseImage.addEventListener('click', function (evt) {
-    const Image = evt.target.closest('.popup');
-    closePopup(Image);
+    const image = evt.target.closest('.popup');
+    closePopup(image);
 })
 // -----------------------------Увеличение картинки--------------------------------
 
 // -----------------------Профиль---------------------
-function openform() {
-    popupName.setAttribute('value', name.innerText);
+function openProfilePopup() {
+    popupName.setAttribute('value', profileName.innerText);
     popupDescription.setAttribute('value', description.innerText);
     openPopup(popup);
 }
 
-function closeform() {
+function closeProfilePopup() {
     closePopup(popup);
 }
 
-function formSubmitHandler (evt) {
+function handleProfileSubmit (evt) {
     evt.preventDefault();   
-    name.textContent = nameValue.value;;
+    profileName.textContent = nameValue.value;;
     description.textContent = descriptionValue.value;
-    closeform();
+    closeProfilePopup();
 }
 // ---------------------Профиль--------------------------
 
@@ -130,15 +104,16 @@ function addNewCard (evt){
     evt.preventDefault();
     const inputName = inputCardName.value;
     const inputLink = inputCardLink.value;                              // Новая карточка
-    const newCard = compilationCard({ name: inputName, link: inputLink });
+    const newCard = createCard({ name: inputName, link: inputLink });
     cardList.prepend(newCard);
+    popupContainer.reset();
     closeCardForm();
 }
 
-firstCards();
-editbutton.addEventListener ('click', openform);
-closebutton.addEventListener ('click', closeform);
-popupContainer.addEventListener('submit', formSubmitHandler);
+
+editButton.addEventListener ('click', openProfilePopup);
+closeButtonProfile.addEventListener ('click', closeProfilePopup);
+popupContainer.addEventListener('submit', handleProfileSubmit);
 popupCardOpen.addEventListener('click', () => openPopup(popupCard));
 popupCardClose.addEventListener('click', closeCardForm);
 saveButtonCard.addEventListener('click', addNewCard);
---
+++ scripts/initial-сards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+        name: 'Балаково',
+        link: 'https://balakovo24.ru/wp-content/uploads/2019/07/погода_балаково.jpg'
+    },
+    {
+        name: 'Архангельск',
+        link: 'http://sail-friend.ru/gallery/3211_17_04_16_8_56_13.jpeg'
+    },
+    {
+        name: 'Санкт-Петербург',
+        link: 'https://gullivertour.ru/assets/images/City/sankt-peterburg/s1200.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+]; 
